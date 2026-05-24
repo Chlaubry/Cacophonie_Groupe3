8e77d25f58c921453dbfbc195ccecee1f38c1d27
chlaubry <caubry@enssat.fr>
2026-05-24 16:20:18 +0200

    Code pour le bot perroquet fonctionnel dans index_test_a

--- bootstrapCodeForDiscordBot/index_test_a.js
+++ bootstrapCodeForDiscordBot/index_test_a.js
@@ -1,36 +1,35 @@
-// Require the necessary discord.js classes
-const { Client,  Events, GatewayIntentBits } = require('discord.js');
-const { token, salon_id } = require('./config_test_a.json');
+require('dotenv').config();
+const { Client, Events, GatewayIntentBits } = require('discord.js');
 
-// Create a new client instance
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent,GatewayIntentBits.GuildMembers,] });
+const token = process.env.BOT_TOKEN2;
+const salon_id = process.env.CHANNEL_ID;
 
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ],
+});
 
-const channel = client.channels.cache.get(salon_id);
+client.once(Events.ClientReady, async (readyClient) => {
+    console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+
+    const channel = await client.channels.fetch(salon_id);
+
+    if (channel) {
+        await channel.send('Bot is ready!');
+    }
+});
 
 client.on('messageCreate', async (message) => {
 
-    console.log("Je viens de lire un message")
-    if (message.channel.id === salon_id) {
-        console.log("il est bien dans mon channel")
-        console.log(`son auteur est ${message.author.tag} et moi je suis ${client.user.tag}`)
-        
-        if(message.author.tag != client.user.tag){
-            const channel = client.channels.cache.get(salon_id);
-            channel.send(message.content);
-        }
-        //
-    }
-}) 
+    if (message.channel.id !== salon_id) return;
+    if (message.author.bot) return;
 
+    console.log("Je viens de lire un message");
 
-
-client.once(Events.ClientReady, readyClient => {
-	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
-     
-    const channel = client.channels.cache.get(salon_id);
-    channel.send('Bot is ready!');
+    await message.channel.send(message.content);
 });
 
-// Log in to Discord with your client's token
 client.login(token);
